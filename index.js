2a1cd7f53a470939e7b75199d82a1e0c7d8afbb0
Sayialel <sayialelkantai7@gmail.com>
2024-01-09 18:28:42 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+var multiply = num1 * num2;
+multiply = 62
+let num1= 62
+let num2= 1
+
+var random = Math.floor(Math.random() * 10) + 1;
+random = 8
+
+var mod = num3 % num4;
+mod = 4
+let num3 = 16
+let num4 = 6
+
+var max = 20
+set = 4, 8, 12, 16, 20
+math.max(set)
+
